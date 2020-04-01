7a090cf936a7c7eee9c3d5a9e691aa04650ad2ca
npnguyen <npnguyen@axon.com>
2020-04-02 01:12:55 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ gulp.task("licenses", async function() {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
-    .pipe(gap.prependText())
+    .pipe(gap.prependText(""))
     .pipe(gulp.dest("./", { overwrite: true }));
 
   // this is to add Creative Tim licenses in the production mode for the minified html
   gulp
     .src("build/index.html", { base: "./" })
-    .pipe(gap.prependText())
+    .pipe(gap.prependText(""))
     .pipe(gulp.dest("./", { overwrite: true }));
 
   // this is to add Creative Tim licenses in the production mode for the minified css
   gulp
     .src("build/static/css/*chunk.css", { base: "./" })
-    .pipe(gap.prependText())
+    .pipe(gap.prependText(""))
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
 });
