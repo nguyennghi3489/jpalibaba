f3d4a9d718f0444870aa2ce4e61468ecbe40d2b8
npnguyen <npnguyen@axon.com>
2020-08-12 22:35:08 +0700

    revert

--- src/provider/apis/base.ts
+++ src/provider/apis/base.ts
@@ -16,7 +16,7 @@ export const callApi = async <T>(
 
   let options: RequestInit = {
     method: method, // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
+    // mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "same-origin", // include, *same-origin, omit
     headers: header,
